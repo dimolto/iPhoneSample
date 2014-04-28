f737d7f6c1d17e7b309a4d1c8f56f46b4c23c8f1
Oshiro Nobuyasu <nobuyasu@Oshiros-MacBook-Pro.local>
2014-04-29 00:25:13 +0900

    Added sample code for UILabel

--- iPhoneSample/ViewController.m
+++ iPhoneSample/ViewController.m
@@ -9,6 +9,7 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *myLabel;
 
 @end
 
@@ -17,13 +18,20 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    [[self myLabel] setText:@"myLabel"];
+    [[self myLabel] setTextAlignment:NSTextAlignmentCenter];
+    [[self myLabel] setTextColor:[UIColor whiteColor]];
+    [[self myLabel] setBackgroundColor:[UIColor blackColor]];
+    [[self myLabel] setFont:[UIFont fontWithName:@"System Bold" size:18.0]];
+    
+    
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+    
 }
 
 @end
