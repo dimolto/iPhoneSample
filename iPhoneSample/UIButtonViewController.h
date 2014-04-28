6a0853ed3328c2989da5e3b95deb816b6424373f
Oshiro Nobuyasu <nobuyasu@Oshiros-MacBook-Pro.local>
2014-04-29 03:57:15 +0900

    Added UIButtonViewController

---
+++ iPhoneSample/UIButtonViewController.h
@@ -0,0 +1,13 @@
+//
+//  UIButtonViewController.h
+//  iPhoneSample
+//
+//  Created by Oshiro Nobuyasu on 4/29/14.
+//  Copyright (c) 2014 Oshiro Nobuyasu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIButtonViewController : UIViewController
+
+@end
---
+++ iPhoneSample/UIButtonViewController.m
@@ -0,0 +1,61 @@
+//
+//  UIButtonViewController.m
+//  iPhoneSample
+//
+//  Created by Oshiro Nobuyasu on 4/29/14.
+//  Copyright (c) 2014 Oshiro Nobuyasu. All rights reserved.
+//
+
+#import "UIButtonViewController.h"
+
+@interface UIButtonViewController ()
+- (IBAction)prevButton:(id)sender;
+
+
+@end
+
+@implementation UIButtonViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+- (void)viewDidAppear:(BOOL)animated{
+
+}
+
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (IBAction)prevButton:(id)sender {
+    [self performSegueWithIdentifier:@"toFirstView" sender:self];
+    NSLog(@"prev Button");
+
+    
+}
+@end
