9091a68e4b5e96122e74979c8bc4a27e11687c64
Oshiro Nobuyasu <nobuyasu@Oshiros-MacBook-Pro.local>
2014-04-29 02:15:04 +0900

    Added Recognizer in ViewController

--- iPhoneSample/ViewController.m
+++ iPhoneSample/ViewController.m
@@ -10,6 +10,8 @@
 
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UILabel *myLabel;
+@property (nonatomic) NSUInteger count;
+- (IBAction)myRecognizer:(id)sender;
 
 @end
 
@@ -18,14 +20,33 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+
+    self.count = 0;
     [[self myLabel] setText:@"myLabel"];
     [[self myLabel] setTextAlignment:NSTextAlignmentCenter];
     [[self myLabel] setTextColor:[UIColor whiteColor]];
     [[self myLabel] setBackgroundColor:[UIColor blackColor]];
     [[self myLabel] setFont:[UIFont fontWithName:@"System Bold" size:18.0]];
-    
-    
+
 }
+
+-(IBAction)myRecognizer:(id)sender{
+    self.count = self.count + 1;
+    [[self myLabel] setText:[[NSString alloc]initWithFormat:@"TAP %d", self.count]];
+}
+
+/*
+ - (void)viewDidAppear:(BOOL)animated{
+//    [self changeToButtonView];
+}
+- (void)changeToButtonView {
+    UIButtonViewController *buttonViewController = [[UIButtonViewController alloc]initWithNibName:@"UIButtonView" bundle:nil];
+    [buttonViewController setModalTransitionStyle:UIModalTransitionStyleCoverVertical];
+    [self presentViewController:buttonViewController animated:YES completion:^(){
+        NSLog(@"complete");
+    }];
+}
+*/
 
 - (void)didReceiveMemoryWarning
 {
